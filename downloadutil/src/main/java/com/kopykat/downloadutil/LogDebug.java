73d482d62ed42702df0371179f2b9035c4143318
Nissen <n.parianen@gmail.com>
2018-03-11 15:43:43 +0100

    Initial library setup

---
+++ downloadutil/src/main/java/com/kopykat/downloadutil/LogDebug.java
@@ -0,0 +1,14 @@
+package com.kopykat.downloadutil;
+
+import android.util.Log;
+
+/**
+ * Created by zero on 11.03.18.
+ */
+
+public class LogDebug {
+    private  static final  String TAG = "Testing custom library";
+    public  static  void  d(String message){
+        Log.d(TAG, message);
+    }
+}
